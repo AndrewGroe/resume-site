f902b5213b2f8474d410d184979e805007244dc1
Andrew Groe <43552026+AndrewGroe@users.noreply.github.com>
2023-01-27 13:02:27 -0500

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -2,20 +2,8 @@
   <v-app>
     <v-card>
       <v-app-bar app>
-        <v-toolbar-title>
-          <span class="headline text-uppercase">Andrew Groe</span>
-          <span class="headline primary--text"> | </span>
-          <span class="subtitle font-weight-thin">Software Developer</span>
-        </v-toolbar-title>
         <v-spacer></v-spacer>
-        <v-btn
-          color="primary"
-          href="https://github.com/AndrewGroe"
-          target="_blank"
-        >
-          <span>My Github</span>
-          <v-icon right>mdi-github-circle</v-icon>
-        </v-btn>
+        
       </v-app-bar>
     </v-card>
 
