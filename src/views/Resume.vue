94c0b05aa8d5adb51ea41330042f6cf9588d22eb
Andrew Groe <43552026+AndrewGroe@users.noreply.github.com>
2023-01-27 12:46:07 -0500

    Update Resume.vue

--- src/views/Resume.vue
+++ src/views/Resume.vue
@@ -16,23 +16,6 @@
           md="8"
           lg="6"
         >
-          <Experience />
-        </v-col>
-
-        <v-col
-          cols="12"
-          sm="12"
-          md="4"
-          lg="3"
-        >
-          <Skills />
-        </v-col>
-
-        <v-col>
-          <Education />
-        </v-col>
-        <v-col>
-          <References />
         </v-col>
 
       </v-row>
@@ -43,18 +26,10 @@
 
 <script>
 import Bio from "../components/Bio";
-import Experience from "../components/Experience";
-import Skills from "../components/Skills";
-import References from "../components/References";
-import Education from "../components/Education";
 
 export default {
   components: {
-    Bio,
-    Experience,
-    Skills,
-    References,
-    Education
+    Bio
   }
 };
 </script>
